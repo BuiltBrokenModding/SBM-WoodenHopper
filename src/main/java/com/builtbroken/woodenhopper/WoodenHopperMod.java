baebbd29e31cebff2fdb7c7e4db7a0a47e0db97e
GreenCafe <worldbrodcast@gmail.com>
2016-11-15 15:07:07 -0700

    last commit did not take the code within the main class for some reason

--- src/main/java/com/builtbroken/woodenhopper/WoodenHopperMod.java
+++ src/main/java/com/builtbroken/woodenhopper/WoodenHopperMod.java
@@ -1,7 +1,36 @@
 package com.builtbroken.woodenhopper;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+
 /**
- * Created by world on 11/15/2016.
+ * Simple mod to add wooden hoppers to MC.
+ * Created by Green on 11/15/2016.
  */
-public class WoodenHopperMod {
+
+@Mod(modid = WoodenHopperMod.DOMAIN, name = "Wooden Hopper Mod", version = "@MAJOR@.@MINOR@.@REVIS@.@BUILD@")
+public class WoodenHopperMod
+{
+
+    public static final String DOMAIN = "woodenhopper";
+    public static final String PREFIX = DOMAIN + ":";
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
 }
