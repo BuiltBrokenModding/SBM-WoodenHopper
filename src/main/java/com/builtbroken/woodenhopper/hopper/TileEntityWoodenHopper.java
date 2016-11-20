cd2e773c859df24fafde8826eda37f07e3d70735
GreenCafe <worldbrodcast@gmail.com>
2016-11-19 19:29:04 -0700

    More work on the mod

--- src/main/java/com/builtbroken/woodenhopper/hopper/TileEntityWoodenHopper.java
+++ src/main/java/com/builtbroken/woodenhopper/hopper/TileEntityWoodenHopper.java
@@ -1,29 +1,164 @@
 package com.builtbroken.woodenhopper.hopper;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntityHopper;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.IChatComponent;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 /**
  * Created by world on 11/15/2016.
  */
-public class TileEntityWoodenHopper extends TileEntityHopper {
+public class TileEntityWoodenHopper extends TileEntityHopper implements IInventory{
+
+    private ItemStack[] inventory;
+    private String customName;
 
     public TileEntityWoodenHopper()
     {
-        super();
+        this.inventory = new ItemStack[this.getSizeInventory()];
+
+    }
+
+    public String getCustomName()
+    {
+        return this.customName;
+    }
+
+    public void setCustomName(String customName) {
+        this.customName = customName;
+    }
+
+    public String getName() {
+        return this.hasCustomName() ? this.customName : "container.wooden_hopper_tile_entity";
+    }
+
+    public boolean hasCustomName() {
+        return this.customName != null && !this.customName.equals("");
+    }
+
+    public IChatComponent getDisplayName() {
+        return this.hasCustomName() ? new ChatComponentText(this.getName()) : new ChatComponentTranslation(this.getName());
     }
 
     @Override
-    public int getSizeInventory()
-    {
+    public int getSizeInventory() {
         return 1;
     }
 
     @Override
-    public String getInventoryName()
-    {
-        return "Wooden Hopper";
+    public ItemStack getStackInSlot(int index) {
+        if (index < 0 || index >= this.getSizeInventory())
+            return null;
+        return this.inventory[index];
     }
+
+    @Override
+    public ItemStack decrStackSize(int index, int count) {
+        if (this.getStackInSlot(index) != null) {
+            ItemStack itemstack;
+
+            if (this.getStackInSlot(index).stackSize <= count) {
+                itemstack = this.getStackInSlot(index);
+                this.setInventorySlotContents(index, null);
+                this.markDirty();
+                return itemstack;
+            } else {
+                itemstack = this.getStackInSlot(index).splitStack(count);
+
+                if (this.getStackInSlot(index).stackSize <= 0) {
+                    this.setInventorySlotContents(index, null);
+                } else {
+                    //Just to show that changes happened
+                    this.setInventorySlotContents(index, this.getStackInSlot(index));
+                }
+
+                this.markDirty();
+                return itemstack;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int index) {
+        ItemStack stack = this.getStackInSlot(index);
+        this.setInventorySlotContents(index, null);
+        return stack;
+    }
+
+    @Override
+    public void setInventorySlotContents(int index, ItemStack stack) {
+        if (index < 0 || index >= this.getSizeInventory())
+            return;
+
+        if (stack != null && stack.stackSize > this.getInventoryStackLimit())
+            stack.stackSize = this.getInventoryStackLimit();
+
+        if (stack != null && stack.stackSize == 0)
+            stack = null;
+
+        this.inventory[index] = stack;
+        this.markDirty();
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return 64;
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int index, ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound nbt) {
+        super.writeToNBT(nbt);
+
+        NBTTagList list = new NBTTagList();
+        for (int i = 0; i < this.getSizeInventory(); ++i) {
+            if (this.getStackInSlot(i) != null) {
+                NBTTagCompound stackTag = new NBTTagCompound();
+                stackTag.setByte("Slot", (byte) i);
+                this.getStackInSlot(i).writeToNBT(stackTag);
+                list.appendTag(stackTag);
+            }
+        }
+        nbt.setTag("Items", list);
+
+        if (this.hasCustomName()) {
+            nbt.setString("CustomName", this.getCustomName());
+        }
+    }
+
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbt) {
+        super.readFromNBT(nbt);
+
+        NBTTagList list = nbt.getTagList("Items", 10);
+        for (int i = 0; i < list.tagCount(); ++i) {
+            NBTTagCompound stackTag = list.getCompoundTagAt(i);
+            int slot = stackTag.getByte("Slot") & 255;
+            this.setInventorySlotContents(slot, ItemStack.loadItemStackFromNBT(stackTag));
+        }
+
+        if (nbt.hasKey("CustomName", 8)) {
+            this.setCustomName(nbt.getString("CustomName"));
+        }
+    }
+
+
+
+
 
 }
